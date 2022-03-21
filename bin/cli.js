dcb241847f57d934e5a4c3b17a421f57c67a4642
liuxingyun <liuxingyun2010@gmail.com>
2022-03-21 23:51:55 +0800

    切换模板

--- bin/cli.js
+++ bin/cli.js
@@ -1,33 +1,39 @@
 #! /usr/bin/env node
 
-const chalk = require('chalk')
-const program = require('commander')
-const create = require('../lib/create')
-const figlet = require('figlet')
+const chalk = require("chalk");
+const program = require("commander");
+const create = require("../lib/create");
+const figlet = require("figlet");
 
 program
-  .command('create <app-name>')
-  .description('create a new app')
-  .option('-f --force', 'overwrite target directory if it exists')
+  .command("create <app-name>")
+  .description("create a new app")
+  .option("-f --force", "overwrite target directory if it exists")
   .action((name, options) => {
-    create(name, options)
-  })
+    create(name, options);
+  });
 
 program
-  .version(`v${require('../package.json').version}`, '-v --version')
-  .usage('<command> [option]')
+  .version(`v${require("../package.json").version}`, "-v --version")
+  .usage("<command> [option]");
 
-program
-  .on('--help', () => {
-    console.log(`\r\n` + figlet.textSync('lxy', {
-      font: 'Ghost',
-      width: 80,
-      whitespaceBreak: true,
-      horizontalLayout:'default',
-      verticalLayout: 'default'
-    }))
+program.on("--help", () => {
+  console.log(
+    `\r\n` +
+      figlet.textSync("ffe", {
+        font: "Ghost",
+        width: 80,
+        whitespaceBreak: true,
+        horizontalLayout: "default",
+        verticalLayout: "default",
+      })
+  );
 
-    console.log(`\r\nRun ${chalk.cyan('xy <commander> --help')} for detailed usage of given command`)
-  })
+  console.log(
+    `\r\nRun ${chalk.cyan(
+      "ffe <commander> --help"
+    )} for detailed usage of given command`
+  );
+});
 
-program.parse(process.argv)
+program.parse(process.argv);
